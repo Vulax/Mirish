8b77f71df4bdd3c10bbd602b9dfa67859857458f
Vulax <vukajlotodorovic@gmail.com>
2024-06-09 12:30:30 +0200

    changed image src path, configured npm build command with base href so that local build can work

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { AfterViewInit,  Component, ElementRef, Inject, Renderer2, ViewChild } from '@angular/core';
 import { ProductService } from '../item/product.service';
 import { Router } from '@angular/router';
 import { UserService } from '../auth/user.service';
@@ -14,7 +14,7 @@ export class MainComponent implements AfterViewInit {
   items = [
     { image: 'assets/images/1.jpg' }
   ];
-  constructor(public productService: ProductService, private router:Router, public userService: UserService, public renderer: Renderer2){
+  constructor(public productService: ProductService, private router:Router, public userService: UserService, @Inject(Renderer2) public renderer: Renderer2){
   }
 
   setHeight() {
